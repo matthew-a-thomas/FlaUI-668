9e5e92e4bfcf34318e58efa0606a642153ece284
Matthew Thomas <me@matthewathomas.com>
2025-01-09 11:42:31 -0500

    Demonstrate https://github.com/FlaUI/FlaUI/issues/668

---
+++ src/ScrollTest/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ScrollTest;
+
+using System;
+using FlaUI.Core.Input;
+
+class Program
+{
+    static async Task Main()
+    {
+        while (true)
+        {
+            Mouse.Scroll(-1);
+            Console.WriteLine("Scroll");
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+}
