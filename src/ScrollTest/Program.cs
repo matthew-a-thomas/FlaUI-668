152e2263aaeae64f77ec3589bb9afe42b38e51aa
Matthew Thomas <me@matthewathomas.com>
2025-01-09 12:54:40 -0500

    Prove that the issue lies outside of FlaUI

--- src/ScrollTest/Program.cs
+++ src/ScrollTest/Program.cs
@@ -1,18 +1,42 @@
 ﻿namespace ScrollTest;
 
 using System;
-using FlaUI.Core.Input;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using Windows.Win32;
+using Windows.Win32.UI.Input.KeyboardAndMouse;
 
 class Program
 {
-    static async Task Main()
+    static void Main()
     {
         while (true)
         {
-            Mouse.Scroll(-1);
             Console.WriteLine("Scroll");
+            Scroll(-1);
 
-            await Task.Delay(TimeSpan.FromSeconds(1));
+            Thread.Sleep(1000);
+        }
+    }
+
+    static void Scroll(double lines)
+    {
+        var amount = (uint)(PInvoke.WHEEL_DELTA * lines);
+        var mouseInput = new MOUSEINPUT{
+            dx = 0,
+            dy = 0,
+            mouseData = amount,
+            dwExtraInfo = 0,
+            time = 0,
+            dwFlags = MOUSE_EVENT_FLAGS.MOUSEEVENTF_WHEEL
+        };
+
+        var input = new INPUT{ type = INPUT_TYPE.INPUT_MOUSE, Anonymous = { mi = mouseInput }};
+        Span<INPUT> inputs = stackalloc INPUT[1];
+        inputs[0] = input;
+        if (PInvoke.SendInput(inputs, Marshal.SizeOf<INPUT>()) == 0)
+        {
+            throw new Win32Exception();
         }
     }
 }
